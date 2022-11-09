cc226ae6b9cdabd56f937e92c03098dc60f07cec
Wang Hao <237466265@qq.com>
2022-11-09 18:42:17 +0800

    second

---
+++ lab4_phaser/2022NKUCS-Compilers-Lab-lab5/src/Ast.cpp
@@ -0,0 +1,161 @@
+#include "Ast.h"
+#include "SymbolTable.h"
+#include <string>
+#include "Type.h"
+
+extern FILE *yyout;
+int Node::counter = 0;
+
+Node::Node()
+{
+    seq = counter++;
+}
+
+void Ast::output()
+{
+    fprintf(yyout, "program\n");
+    if(root != nullptr)
+        root->output(4); // root的类型开始是Node*，但是它被实例化了，于是会调用具体类型的output()
+}
+
+void BinaryExpr::output(int level)
+{
+    std::string op_str;
+    // TODO here!!
+    // 多加点运算符的token
+    switch(op)
+    {
+        case NOT:
+            op_str = "not";
+            break;
+        case OR:
+            op_str = "or";
+            break;
+        case AND:
+            op_str = "and";
+            break;
+        case EQUAL:
+            op_str = "equal";
+            break;
+        case NOTEQUAL:
+            op_str = "notEqual";
+            break;
+        case LESS:
+            op_str = "less";
+            break;
+        case LESSEQUAL:
+            op_str = "lessEqual";
+            break;
+        case GREAT:
+            op_str = "great";
+            break;
+        case GREATEQUAL:
+            op_str = "greatEqual";
+            break;
+        case ADD:
+            op_str = "add";
+            break;
+        case SUB:
+            op_str = "sub";
+            break;
+        case MOD:
+            op_str = "mod";
+            break;
+        case ASSIGN:
+            op_str = "assign";
+            break;
+        case MUL:
+            op_str = "mul";
+            break;
+        case DIV:
+            op_str = "div";
+            break;
+    }
+    fprintf(yyout, "%*cBinaryExpr\top: %s\n", level, ' ', op_str.c_str());
+    expr1->output(level + 4);
+    expr2->output(level + 4);
+}
+
+void Constant::output(int level)
+{
+    std::string type, value;
+    type = symbolEntry->getType()->toStr();
+    value = symbolEntry->toStr();
+    fprintf(yyout, "%*cIntegerLiteral\tvalue: %s\ttype: %s\n", level, ' ',
+            value.c_str(), type.c_str());
+}
+
+void Id::output(int level)
+{
+    std::string name, type;
+    int scope;
+    name = symbolEntry->toStr();
+    type = symbolEntry->getType()->toStr();
+    scope = dynamic_cast<IdentifierSymbolEntry*>(symbolEntry)->getScope();
+    fprintf(yyout, "%*cId\tname: %s\tscope: %d\ttype: %s\n", level, ' ',
+            name.c_str(), scope, type.c_str());
+}
+
+void CompoundStmt::output(int level)
+{
+    fprintf(yyout, "%*cCompoundStmt\n", level, ' ');
+    stmt->output(level + 4);
+}
+
+void SeqNode::output(int level)
+{
+    // fprintf(yyout, "%*cSequence\n", level, ' ');
+    stmt1->output(level);
+    stmt2->output(level);
+}
+
+void DeclStmt::output(int level)
+{
+    fprintf(yyout, "%*cDeclStmt\n", level, ' ');
+    id->output(level + 4);
+}
+
+void IfStmt::output(int level)
+{
+    fprintf(yyout, "%*cIfStmt\n", level, ' ');
+    cond->output(level + 4);
+    thenStmt->output(level + 4);
+}
+
+void IfElseStmt::output(int level)
+{
+    fprintf(yyout, "%*cIfElseStmt\n", level, ' ');
+    cond->output(level + 4);
+    thenStmt->output(level + 4);
+    elseStmt->output(level + 4);
+}
+
+void WhileStmt::output(int level)
+{
+    fprintf(yyout, "%*cWhileStmt\n", level, ' '); // 打印level个'*'
+    cond->output(level + 4); // 调用的是ExprNode子类中的BinaryExpr（这也是cond的类型，且仅在本程序中是BinaryExpr）中的output()
+    whileStmt->output(level + 4); // 调用的是StmtNode子类中的CompoundStmt中的output（因为本程序中的whileStmt是CompoundStmt类型的）
+}
+
+void ReturnStmt::output(int level)
+{
+    fprintf(yyout, "%*cReturnStmt\n", level, ' ');
+    retValue->output(level + 4);
+}
+
+void AssignStmt::output(int level)
+{
+    fprintf(yyout, "%*cAssignStmt\n", level, ' ');
+    lval->output(level + 4);
+    expr->output(level + 4);
+}
+
+void FunctionDef::output(int level)
+{
+    std::string name, type;
+    name = se->toStr();
+    type = se->getType()->toStr();
+    fprintf(yyout, "%*cFunctionDefine function name: %s, type: %s\n", level, ' ', 
+            name.c_str(), type.c_str());
+    stmt->output(level + 4);
+}
